e0b57021dc1591d5dd9c0acdb441c33b2c3f1ad9
MaximTuktarev <maxim.tuktarev@mail.ru>
2022-04-19 19:49:00 +0400

    clean needless

--- src/test/java/ru/netology/data/DataGenerator.java
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -12,60 +12,60 @@ import java.util.Locale;
 import static io.restassured.RestAssured.given;
 
 
-    public class DataGenerator {
-        private static final Faker faker = new Faker(new Locale("en"));
-        private static final RequestSpecification requestSpec = new RequestSpecBuilder()
-                .setBaseUri("http://localhost")
-                .setPort(9999)
-                .setAccept(ContentType.JSON)
-                .setContentType(ContentType.JSON)
-                .log(LogDetail.ALL)
-                .build();
+public class DataGenerator {
+    private static final Faker faker = new Faker(new Locale("en"));
+    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
 
 
-        private DataGenerator() {
-        }
-
-        public static void sendRequest(RegistrationDto registeredUser) {
-            given()
-                    .spec(requestSpec)
-                    .body(registeredUser)
-                    .when()
-                    .post("/api/system/users")
-                    .then()
-                    .statusCode(200);
-        }
-
-        public static String getRandomLogin() {
-            return faker.name().username();
-        }
-
-        public static String getRandomPassword() {
-            return faker.internet().password();
-        }
-
-        public static class Registration {
-            private Registration() {
-            }
-
-            public static RegistrationDto getUser(String status) {
-                return new RegistrationDto(getRandomLogin(), getRandomPassword(), status);
-            }
-
-            public static RegistrationDto getRegisteredUser(String status) {
-                var registeredUser = getUser(status);
-                sendRequest(registeredUser);
-                return registeredUser;
-            }
-
-
-        }
-
-        @Value
-        public static class RegistrationDto {
-            String login;
-            String password;
-            String status;
-        }
+    private DataGenerator() {
     }
+
+    public static void sendRequest(RegistrationDto registeredUser) {
+        given()
+                .spec(requestSpec)
+                .body(registeredUser)
+                .when()
+                .post("/api/system/users")
+                .then()
+                .statusCode(200);
+    }
+
+    public static String getRandomLogin() {
+        return faker.name().username();
+    }
+
+    public static String getRandomPassword() {
+        return faker.internet().password();
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static RegistrationDto getUser(String status) {
+            return new RegistrationDto(getRandomLogin(), getRandomPassword(), status);
+        }
+
+        public static RegistrationDto getRegisteredUser(String status) {
+            var registeredUser = getUser(status);
+            sendRequest(registeredUser);
+            return registeredUser;
+        }
+
+
+    }
+
+    @Value
+    public static class RegistrationDto {
+        String login;
+        String password;
+        String status;
+    }
+}
 
